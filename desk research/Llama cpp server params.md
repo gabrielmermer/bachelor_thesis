ab29825ae8e2e8e7bc9949648a7e18e4d3d5447d
gabrielmermer <zubrzysta@gmail.com>
2025-06-23 18:22:37 +0900

    super basic tool call example

--- desk research/Llama cpp server params.md
+++ desk research/Llama cpp server params.md
@@ -1,2 +1,2 @@
 
-./llama-server -m ../../models/ToolACE-2-Llama-3.1-8B-Q4_K_M.ggml --n-gpu-layers 40 --host 0.0.0.0
+./llama-server -m ../../models/ToolACE-2-Llama-3.1-8B-Q4_K_M.ggml --n-gpu-layers 60 --host 0.0.0.0 --jinja
--- mvp/mvp.py
+++ mvp/mvp.py
@@ -1,10 +1,38 @@
 import httpx
 import json
 
-url = "http://100.126.176.4:8080/completion"
+url = "http://100.126.176.4:8080/v1/chat/completions"
+
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_weather",
+            "description": "Get current temperature for a given location.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "City and country e.g. Bogotá, Colombia"
+                    }
+                },
+                "required": ["location"]
+            }
+        }
+    }
+]
+
 payload = {
-    "prompt": "Is learning polish hard?",
-    "n_predict": 50 # the ammount of tokens we're predicting  
+    "model": "gpt-3.5-turbo",
+    "tools": tools,
+    "messages": [
+        {
+            "role": "user",
+            "content": "What is the weather in Seoul?"
+        }
+    ]
 }
 
 llm_response = httpx.post(url, json=payload, timeout=120.0)
@@ -12,8 +40,8 @@ llm_response = httpx.post(url, json=payload, timeout=120.0)
 
 
 print("Status Code:", llm_response.status_code)
-print(llm_response.text)
+# print(llm_response.text)
 
 response_json = json.loads(llm_response.text)
-print(type(response_json))
-print(response_json["content"])
+# print(type(response_json))
+print(response_json)
