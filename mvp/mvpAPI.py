2f1df35d64ac9832c29b6a1fff4cd2b2e503d4cd
gabrielmermer <zubrzysta@gmail.com>
2025-06-24 14:49:18 +0900

    basic ffmpeg audio processing

--- mvp/mvpAPI.py
+++ mvp/mvpAPI.py
@@ -1,14 +1,28 @@
-from fastapi import FastAPI, Request
+# local API endpoint
+from fastapi import FastAPI, Request, File, UploadFile
 # for the request body
 from pydantic import BaseModel
+# llama.cpp backend request
 import httpx
+
 import json
+
+# temp audio file integration
+import shutil
+# running local cli subprocesses
+import subprocess
+# temp file 
+import tempfile
+
 
 app = FastAPI()
 
 
 class Prompt(BaseModel):
     prompt: str
+
+class VoicePrompt(BaseModel):
+    prompt: object
     
 
 
@@ -54,3 +68,33 @@ async def process_prompt(prompt: Prompt):
     llm_function_call_name = response_json["choices"][0]["message"]["tool_calls"][0]["function"]["name"]
     llm_function_call_param = response_json["choices"][0]["message"]["tool_calls"][0]["function"]["arguments"]
     return llm_function_call_name, llm_function_call_param
+
+@app.post("/process_audio")
+async def process_audio(audio_file: UploadFile):
+    print(type(audio_file))
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp:
+        shutil.copyfileobj(audio_file.file, tmp)
+        tmp_path = tmp.name
+        print(tmp_path)
+
+        # the needed file conversion for whisper.cpp transcription
+        # ffmpeg -i input.mp3 -ar 16000 -ac 1 -c:a pcm_s16le output.wav
+        # I know this looks like a mess but it's needed
+        # the processed_audio_file is ready to be passed to whisper.cpp server
+        processed_audio_file = subprocess.run([
+            "ffmpeg", "-y",
+            "-i", tmp_path,
+            "-ar",
+            "16000",
+            "-ac",
+            "1",
+            "-c:a",
+            "pcm_s16le",
+            "output.wav"])
+        
+
+
+
+    return {"filename": processed_audio_file}
+
+    
