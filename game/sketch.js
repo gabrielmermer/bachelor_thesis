b35bc63b5f2344ac79a3a270b074b9be69b6371c
Gabriel Mermer <zubrzysta@gmail.com>
2026-03-10 17:49:57 +0100

    -1F and 0F partial description update

--- game/sketch.js
+++ game/sketch.js
@@ -124,7 +124,7 @@ function setup() {
   console.log("3 - recipes done");
   
   // game.currentLocation = game.locations.location_2F_rooftop;
-  game.currentLocation = game.locations.location_minus1F_parking;
+  game.currentLocation = game.locations.location_0F_entrance_ground;
   console.log("4 - current location set");
 
   noStroke();
