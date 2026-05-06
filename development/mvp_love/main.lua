f23bb1db9d599524c967e4ab4f6c5c95f238ca55
Gabriel Mermer <zubrzysta@gmail.com>
2026-05-06 16:44:55 +0200

    cleanup

--- development/mvp_love/main.lua
+++
@@ -1,9 +0,0 @@
-function love.load()
-	font = love.graphics.newFont(16)
-    font:setFilter("nearest","nearest")
-end
-
-function love.draw()
-	love.graphics.setFont(font)
-	love.graphics.print("Hello, World!", 200, 100)
-end
